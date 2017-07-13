72f598769a3281eafdbce3584240cf8c9d790452
BumbuKhan <qurban.qurbanov93@gmail.com>
2017-07-14 00:11:00 +0400

    Added simple data inserting from the form to the database

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
-var mysql = require('mysql')
+var mysql = require('mysql');
 var connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database : process.env.DB_NAME
-})
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+});
 
-connection.connect()
+connection.connect();
 
 module.exports = connection;
--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,24 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-    res.render('index', {title: 'Registration'});
+router.get('/register', function (req, res, next) {
+    res.render('register', {title: 'Registration'});
 });
 
 router.post('/register', function (req, res, next) {
-    res.render('index', {title: 'Registration complete'});
+    const username = req.body.username;
+    const email = req.body.email;
+    const password = req.body.password;
+
+    const db = require('../db.js');
+
+    db.query('INSERT INTO users (username, email, password_hash) VALUES (?, ?, ?)', [username, email, password], function (err, result, fields) {
+        if (err) {
+            throw err;
+        }
+
+        res.render('register', {title: 'Registration complete'})
+    });
 });
 
 module.exports = router;
