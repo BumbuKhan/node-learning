9158521026214335ed532a90b22b0e097ce7ff46
BumbuKhan <qurban.qurbanov93@gmail.com>
2017-07-14 00:52:45 +0400

    Added data validation

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
+var expressValidator = require('express-validator');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
@@ -20,7 +21,9 @@ app.set('view engine', 'hbs');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(expressValidator()); // this line must be immediately after any of the bodyParser middlewares!
+
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -28,21 +31,21 @@ app.use('/', index);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 
@@ -55,16 +58,16 @@ const partialsDir = __dirname + '/views/partials';
 const filenames = fs.readdirSync(partialsDir);
 
 filenames.forEach(function (filename) {
-  const matches = /^([^.]+).hbs$/.exec(filename);
-  if (!matches) {
-    return;
-  }
-  const name = matches[1];
-  const template = fs.readFileSync(partialsDir + '/' + filename, 'utf8');
-  hbs.registerPartial(name, template);
+    const matches = /^([^.]+).hbs$/.exec(filename);
+    if (!matches) {
+        return;
+    }
+    const name = matches[1];
+    const template = fs.readFileSync(partialsDir + '/' + filename, 'utf8');
+    hbs.registerPartial(name, template);
 });
 
-hbs.registerHelper('json', function(context) {
+hbs.registerHelper('json', function (context) {
     return JSON.stringify(context, null, 2);
 });
 
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var expressValidator = require('express-validator');
 
 /* GET home page. */
 router.get('/register', function (req, res, next) {
@@ -7,19 +8,40 @@ router.get('/register', function (req, res, next) {
 });
 
 router.post('/register', function (req, res, next) {
-    const username = req.body.username;
-    const email = req.body.email;
-    const password = req.body.password;
 
-    const db = require('../db.js');
+    // validation income data
+    req.checkBody('username', 'Username field can not be empty').notEmpty();
+    req.checkBody('username', 'Username length must be 4-15 chars in length').len(4, 15);
+    req.checkBody('email', 'Incorrect email').isEmail();
+    req.checkBody('email', 'Email field length should be 4-100').len(4, 100);
+    req.checkBody('password', 'Password should contain at least lower case letter, upper case letter and special char').matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9]).{8,}$/, "i");
+    req.checkBody('password-repeat', 'Password should be at least 8 char long').len(8, 100);
+    req.checkBody('password-repeat', 'Passwords do not mach').equals(req.body.password);
 
-    db.query('INSERT INTO users (username, email, password_hash) VALUES (?, ?, ?)', [username, email, password], function (err, result, fields) {
-        if (err) {
-            throw err;
-        }
+    const errors = req.validationErrors();
 
-        res.render('register', {title: 'Registration complete'})
-    });
+    if (errors) {
+        console.log(`Errors in form: ${JSON.stringify(errors)}`);
+
+        res.render('register', {
+            title: 'Registration failed, you have an errors',
+            errors: errors
+        })
+    } else {
+        const username = req.body.username;
+        const email = req.body.email;
+        const password = req.body.password;
+
+        const db = require('../db.js');
+
+        db.query('INSERT INTO users (username, email, password_hash) VALUES (?, ?, ?)', [username, email, password], function (err, result, fields) {
+            if (err) {
+                throw err;
+            }
+
+            res.render('register', {title: 'Registration complete'})
+        });
+    }
 });
 
 module.exports = router;
